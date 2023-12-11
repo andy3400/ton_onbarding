b81976f3dcd8677513637cd505855dfda3bb111b
andy3400 <grayhat3400@gmail.com>
2023-12-11 19:58:33 +0900

    Feat: first commit

--- index.ts
+++ index.ts
@@ -1,2 +1,23 @@
+import {Address, TonClient} from "ton"
+import {getHttpEndpoint} from "@orbs-network/ton-access";
 
-// write your NFT miner here
+
+async function main () {
+    
+    const wallet = Address.parse('UQAb9yfMhZ9IiXaUydbOyin55zbCM2vomVRSDkpNztxvFkoB');
+    const collection = Address.parse('UQAb9yfMhZ9IiXaUydbOyin55zbCM2vomVRSDkpNztxvFkoB');
+
+    const endpoint = await getHttpEndpoint({
+        network: "testnet",
+      });
+    
+      // initialize ton library
+    const client = new TonClient({ endpoint });
+
+    const miningData = await client.callGetMethod(collection, 'get_mining_data')
+
+console.log(miningData)
+}
+
+
+main()
